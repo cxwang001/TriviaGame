50463a63c05ff4cfc0c219f7d925a4e53f7949d3
cxwang001 <william@Williams-MBP.attlocal.net>
2017-06-21 19:21:27 -0700

    TriviaGame

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,33 +2,54 @@ $(document).ready(function() {
 
 var rightAnswer = 0;
 var wrongAnswer = 0;
+var noAnswer=0;
 
 	function checkAnswers() {
 
 		$( '.button-question').on('click', function() {
 			var $this = $(this);
-			// console.log($this);
+			console.log($this);
 			var val = $this.attr('value');
 			// console.log(val);
 
 			if (val === 'true'){
 				rightAnswer ++;
-				console.log("right");
 				console.log(rightAnswer);
 			} else {
 				wrongAnswer ++;
-				console.log("wrong");
+				console.log(wrongAnswer);
 			}
+			noAnswer = 5 - (rightAnswer + wrongAnswer);
 		});
 
 	}
+	checkAnswers();
+// checkAnswers();
+
+   				
+   	// 
+	// $(document).on('click', "#submit", function(){
+ //    checkAnswer();
+ //    results();
+	// });
+		
+
+	$('.startButton').on('click', function() {
+    	$('#start').hide();
+    	$('.main-body').show();
+	});
+
+	$('#submit').on('click', function() {
+    	$('.main-body').hide();
+    	$('#results').show();
+    	$('#rightAnswer').html(rightAnswer);
+    	$('#wrongAnswer').html(wrongAnswer);
+    	$('#noAnswer').html(noAnswer);	
+	});
+
+
 	
-checkAnswers();	// }
-
-
-
-
-console.log(rightAnswer);
+    	
 
 });
 
