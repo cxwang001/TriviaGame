cfb28a2f391dfddc315c2494ebb32c0c52ecbc67
cxwang001 <william@Williams-MBP.attlocal.net>
2017-06-21 20:15:28 -0700

    TriviaGame

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
 
+
+//set up Global variables
 var rightAnswer = 0;
 var wrongAnswer = 0;
 var noAnswer=0;
+var number = 50;
 
+    //  Variable that will hold our interval ID when we execute
+    //  the "run" function
+var intervalId;
+
+//function to check rigit and wrong answers
 	function checkAnswers() {
 
 		$( '.button-question').on('click', function() {
@@ -33,23 +41,64 @@ var noAnswer=0;
  //    results();
 	// });
 		
-
+	//When the start button is clicked, start page hide and main trivia page shows up
 	$('.startButton').on('click', function() {
     	$('#start').hide();
     	$('.main-body').show();
 	});
 
+	//when submit button is clicked, trivia page hide and result page shows up and run stop function
 	$('#submit').on('click', function() {
     	$('.main-body').hide();
     	$('#results').show();
     	$('#rightAnswer').html(rightAnswer);
     	$('#wrongAnswer').html(wrongAnswer);
-    	$('#noAnswer').html(noAnswer);	
+    	$('#noAnswer').html(noAnswer);
+    	stop();	
 	});
 
+	    //  The run function sets an interval
+    //  that runs the decrement function once a second.
+   function run() {
+      intervalId = setInterval(decrement, 1000);
+    }
 
+    //  The decrement function.
+    function decrement() {
+
+      //  Decrease number by one.
+      number--;
+
+      //  Show the number in the #show-number tag.
+      $('#timeRemaining').html("<h2>" + number + "</h2>");
+
+
+      //  Once number hits zero...
+      if (number === 0) {
+
+        //  ...run the stop function.
+        stop();
+
+        //  Alert the user that time is up.
+        
+        $('.main-body').hide();
+    	$('#results').show();
+    	$('#rightAnswer').html(rightAnswer);
+    	$('#wrongAnswer').html(wrongAnswer);
+    	$('#noAnswer').html(noAnswer);
+      }
+    }
+
+    //  The stop function
+    function stop() {
+
+      //  Clears our intervalId
+      //  We just pass the name of the interval
+      //  to the clearInterval function.
+      clearInterval(intervalId);
+    }
 	
-    	
+    run();
 
 });
 
